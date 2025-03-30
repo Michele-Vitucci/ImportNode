57070b39e712c137ca4c6d25ff867ce074434bd0
Michele Vitucci <michele.vitucci99@gmail.com>
2025-03-30 16:22:12 +0200

    Add files via upload

---
+++ Importnode/Hello.js
@@ -0,0 +1,6 @@
+function Hello() {
+    console.log("Hello,world.");
+}
+
+// Esportiamo la funzione per poterla usare in altri file
+module.exports = Hello;
---
+++ Importnode/Import.js
@@ -0,0 +1,5 @@
+// Importiamo la funzione definita in funzione.js
+const Hello = require("./Hello");
+
+// Chiamiamo la funzione importata
+Hello();
